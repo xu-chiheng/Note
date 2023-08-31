65e220be8e4b68d26f757c64dbaa62e6a4f79981
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2023-08-31 15:19:33 +0800

    1

--- .bashrc.d/git-tools.sh
+++ .bashrc.d/git-tools.sh
@@ -84,7 +84,8 @@ do_git_backup() {
 			false
 			;;
 	esac \
-	&& {
+	&& case "${backup_command}" in
+		backup_to_all_drives | backup_to_upper_directory )
 			local base="$(basename "$(pwd)")"
 			local tarball="${base}-$(current_datetime)".tar
 			case "${backup_command}" in
@@ -103,7 +104,12 @@ do_git_backup() {
 				esac \
 				&& popd;} \
 			&& time_command sync
-	}
+			;;
+		* )
+			echo "unknown backup_command : ${backup_command}"
+			false
+			;;
+	esac
 }
 
 do_git_commit() {

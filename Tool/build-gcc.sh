c138fac05221e13fea6599dd0ec260915bb6c978
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2023-10-07 23:44:38 +0800

    1

--- Tool/build-gcc.sh
+++ Tool/build-gcc.sh
@@ -31,7 +31,7 @@ PACKAGE="gcc"
 	check_toolchain_build_type_and_set_compiler_flags "$1" "$2"
 
 	# VERSION=13.2.0
-	VERSION=14.0.0 # commit eaa41a6dc127d8d8a38646aaadc37681691fc311 2023-09-29
+	VERSION=14.0.0 # commit bf2e66e3ee27fac4ae81416561dbf6b4d71141a4 2023-10-05
 
 	case "${HOST_TRIPLE}" in
 		x86_64-pc-cygwin )

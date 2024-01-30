f4d53434865828c35c6c465e930e6d0454c86e2e
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-01-30 23:04:07 +0800

    1

--- .bashrc.d/v2ray.sh
+++ .bashrc.d/v2ray.sh
@@ -54,7 +54,25 @@ install_nginx() {
 }
 
 install_caddy() {
-	true
+	# https://caddyserver.com/docs/install
+	if which apt; then
+		# Debian, Ubuntu, Raspbian
+		sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+		curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+		curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+		sudo apt update
+		sudo apt install caddy
+	elif which dnf; then
+		# Fedora, RedHat, CentOS
+		dnf install 'dnf-command(copr)'
+		dnf copr enable @caddy/caddy
+		dnf install caddy
+	elif which pacman; then
+		# Arch Linux, Manjaro, Parabola
+		pacman -Syu caddy
+	fi \
+	&& systemctl start caddy \
+	&& systemctl enable caddy
 }
 
 install_apache() {

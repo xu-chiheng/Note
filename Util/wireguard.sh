77b8e53ea28859ec7f2388e611817d5286e4ce2f
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-02-01 16:01:37 -0500

    1

--- Util/wireguard.sh
+++ Util/wireguard.sh
@@ -1,18 +1,31 @@
 
+# https://www.wireguard.com/quickstart/
+
+install_base_tools() {
+	if which apt; then
+		# Debian, Ubuntu, Raspbian
+		apt update \
+		&& apt install -y iproute2
+	elif which dnf; then
+		# Fedora, RedHat, CentOS
+		dnf makecache \
+		&& dnf install -y iproute
+	elif which pacman; then
+		# Arch Linux, Manjaro, Parabola
+		pacman -Syu iproute
+	fi
+}
 
 # https://www.wireguard.com/install/
 install_wireguard() {
 	if which apt; then
 		# Debian, Ubuntu, Raspbian
-		apt update \
-		&& apt install -y wireguard
+		apt install -y wireguard
 	elif which dnf; then
 		# Fedora, RedHat, CentOS
-		dnf makecache \
-		&& dnf install -y wireguard-tools
+		dnf install -y wireguard-tools
 	elif which pacman; then
 		# Arch Linux, Manjaro, Parabola
 		pacman -Syu wireguard-tools
 	fi
 }
-

b581e01b6cbed3950b03ad7e6948ccdc8d572b08
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-02-07 07:30:49 +0800

    1

---
+++ .bashrc.d/vps.sh
@@ -0,0 +1,110 @@
+
+
+
+# https://www.geeksforgeeks.org/create-a-password-generator-using-shell-scripting/
+# https://www.howtogeek.com/howto/30184/10-ways-to-generate-a-random-password-from-the-command-line/
+# https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
+password_generate() {
+	# openssl rand -help
+	# openssl rand -base64 48
+	# openssl rand -hex 64
+	tr -cd '[:alnum:]' < /dev/urandom | fold -w100 | head -n1
+}
+
+# https://stackoverflow.com/questions/2556190/random-number-from-a-range-in-a-bash-script
+port_number_generate() {
+	shuf -i 10000-65535 -n 1
+}
+
+xray_uuid_generate() {
+	cat '/proc/sys/kernel/random/uuid'
+}
+
+install_base_tools() {
+	if which apt; then
+		# Debian, Ubuntu, Raspbian
+		apt update \
+		&& apt install -y openssl cron socat curl
+	elif which dnf; then
+		# Fedora, RedHat, CentOS
+		dnf makecache \
+		&& dnf install -y openssl cron socat curl
+	elif which pacman; then
+		# Arch Linux, Manjaro, Parabola
+		pacman -Syu openssl cron socat curl
+	fi
+}
+
+start_and_enable_service() {
+	local service="$1"
+
+	systemctl start "${service}"
+	systemctl enable "${service}"
+}
+
+stop_and_disable_service() {
+	local service="$1"
+
+	systemctl stop "${service}"
+	systemctl disable "${service}"
+}
+
+print_default_nic() {
+	ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1
+}
+
+print_ipv4_address() {
+	# curl ifconfig.me
+	ip addr show "$(print_default_nic)" | grep 'inet ' | awk '{print $2}' | sed -E 's,/.*$,,'
+}
+
+# https://teddysun.com/489.html
+# https://github.com/teddysun/across/raw/master/bbr.sh
+# https://v2rayssr.com/bbr.html
+# https://www.437r.com/archives/64
+# https://bbr.me/bbr.html
+# https://www.qinyuanyang.com/post/360.html
+enable_bbr() {
+	sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+	echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+	sysctl -p
+	lsmod | grep bbr
+}
+
+# https://www.techrepublic.com/article/how-to-disable-ipv6-on-linux/
+# https://techdocs.broadcom.com/us/en/ca-enterprise-software/it-operations-management/network-flow-analysis/23-3/installing/system-recommendations-and-requirements/linux-servers/disable-ipv6-networking-on-linux-servers.html
+# https://www.geeksforgeeks.org/how-to-disable-ipv6-in-linux/
+disable_ipv6() {
+	sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
+	sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
+	sed -i '/net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.conf
+	echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+	sysctl -p
+	ip a | grep inet6
+}
+
+# https://www.cyberciti.biz/faq/linux-disable-firewall-command/
+# https://www.tecmint.com/start-stop-disable-enable-firewalld-iptables-firewall/
+disable_firwall() {
+	if which firewalld; then
+		stop_and_disable_service firewalld
+	elif which ufw; then
+		stop_and_disable_service ufw
+	elif which iptables; then
+		stop_and_disable_service iptables
+	fi
+}
+
+# https://docs.aws.amazon.com/linux/al2023/ug/disable-option-selinux.html
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/using_selinux/changing-selinux-states-and-modes_using-selinux
+disable_selinux() {
+	if [ -f /etc/selinux/config ]; then
+		sed -i -e 's/^SELINUX=(enforcing|permissive)$/SELINUX=disabled/g' /etc/selinux/config
+		# after reboot
+		# getenforce
+	fi
+}

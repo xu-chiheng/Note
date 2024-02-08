fd29f4982e762ddf4786edfafb750f52fef00b4d
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-02-08 09:02:35 +0800

    1

--- .bashrc.d/base.sh
+++ .bashrc.d/base.sh
@@ -451,6 +451,9 @@ remove_temp_files() {
 	rm -rf
 }
 
+# https://www.geeksforgeeks.org/create-a-password-generator-using-shell-scripting/
+# https://www.howtogeek.com/howto/30184/10-ways-to-generate-a-random-password-from-the-command-line/
+# https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
 password_generate_one() {
 	# openssl rand -help
 	# openssl rand -base64 48
@@ -458,13 +461,7 @@ password_generate_one() {
 	tr -cd '[:alnum:]' < /dev/urandom | fold -w100 | head -n1
 }
 
-# https://www.geeksforgeeks.org/create-a-password-generator-using-shell-scripting/
-# https://www.howtogeek.com/howto/30184/10-ways-to-generate-a-random-password-from-the-command-line/
-# https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
 password_generate() {
-	# openssl rand -help
-	# openssl rand -base64 48
-	# openssl rand -hex 64
 	for i in $(seq 1 40); do
 		password_generate_one
 	done

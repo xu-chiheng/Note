837f354ee78bfbebd82a79360fe44386cc6c6161
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-03-21 22:40:54 +0800

    1

--- Util/download/Visual_Studio/download.cmd
+++ Util/download/Visual_Studio/download.cmd
@@ -1,5 +1,7 @@
 
 @REM https://learn.microsoft.com/en-us/visualstudio/install/create-an-offline-installation-of-visual-studio
-vs_enterprise.exe --layout "%CD%\vslayout" --lang en-US
+@REM https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
+@REM vs_enterprise.exe --layout "%CD%\vslayout" --lang en-US
+vs_enterprise.exe --layout "%CD%\vslayout" --all --arch all --lang en-US
 
 pause

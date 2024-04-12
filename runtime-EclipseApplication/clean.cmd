ad98fc917a2b6d971ca735b794c73985501e32d4
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-04-13 03:17:41 +0800

    1

--- runtime-EclipseApplication/clean.cmd
+++ runtime-EclipseApplication/clean.cmd
@@ -2,4 +2,4 @@
 
 @set PATH="%CYGWIN_DIR%\bin";%PATH%
 
-@start bash.exe -i clean.sh
+@start bash.exe -i -c "find . -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 rm -rf"
--- runtime-EclipseApplication/clean.sh
+++
@@ -1,2 +0,0 @@
-
-find . -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 rm -rf

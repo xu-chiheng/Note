22b5748184597b0d0f72bce35793a5e70639877b
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-04-13 03:16:05 +0800

    1

--- _private/update.sh
+++ _private/update.sh
@@ -26,7 +26,7 @@ do_update_all_files() {
 		.gnupg/.gitignore
 		.gnupg/gpg.conf
 
-		{eclipse-workspace,runtime-EclipseApplication}/{.gitignore,clean.cmd,clean.sh,cygwin.cmd}
+		{eclipse-workspace,runtime-EclipseApplication}/{.gitignore,clean.cmd}
 
 		IDE/{.gitignore,cygwin.cmd,README.md,patch}
 		IDE/{remove_unneeded_plug-ins,generate_list_of_changed_files}.{cmd,sh,sh.txt}

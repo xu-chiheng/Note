4aef50b0bf04b4fdeff03c833d11ddd930314321
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-09-17 21:26:56 +0800

    1

---
+++ ~git-tools~/cygwin/git_pull_--rebase_downstream.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+@set PATH="%CYGWIN_DIR%\bin";%PATH%
+
+cd ../..
+
+@start mintty.exe --exec bash -i -c "time_command do_git_misc git_pull_--rebase downstream; read;"
---
+++ ~git-tools~/msys/git_pull_--rebase_downstream.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+@set PATH="%MSYS2_DIR%\usr\bin";%PATH%
+@set MSYSTEM=MSYS
+
+cd ../..
+
+@start mintty.exe --exec bash -i -c "time_command do_git_misc git_pull_--rebase downstream; read;"

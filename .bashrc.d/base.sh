2fe70a47a23579788439858b948d1807c7b128e1
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-08-03 15:26:36 +0800

    1

--- .bashrc.d/base.sh
+++ .bashrc.d/base.sh
@@ -431,8 +431,15 @@ open_files_in_editor() {
 				translated_file_paths+=( "$(cygpath -w "${file}")" )
 			done
 			# echo "${translated_file_paths[@]}"
+
+			# https://stackoverflow.com/questions/10564/how-can-i-set-up-an-editor-to-work-with-git-on-windows
+			# https://docs.github.com/en/get-started/git-basics/associating-text-editors-with-git
+			# https://git-scm.com/book/ms/v2/Getting-Started-First-Time-Git-Setup
 			# https://npp-user-manual.org/docs/command-prompt/
-			"$(cygpath -u 'C:\Program Files\Notepad++\notepad++')" -multiInst -nosession -noPlugin "${translated_file_paths[@]}"
+			"$(cygpath -u 'C:\Program Files\Notepad++\notepad++.exe')" -multiInst -notabbar -nosession -noPlugin "${translated_file_paths[@]}"
+
+			# https://stackoverflow.com/questions/30024353/how-can-i-use-visual-studio-code-as-default-editor-for-git
+			# https://docs.github.com/en/get-started/git-basics/associating-text-editors-with-git
 			# https://code.visualstudio.com/docs/configure/command-line
 			# "$(cygpath -u 'C:\Program Files\Microsoft VS Code\Code.exe')" --wait --new-window "${translated_file_paths[@]}"
 			;;

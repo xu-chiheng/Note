d52ff78e5d60216e0030745f2601dd98a4b82558
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-08-04 14:09:09 +0800

    1

--- editor.sh
+++ editor.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# simulate bash -i
+# Simulate interactive shell by sourcing user's shell configuration.
 source ~/.bashrc
 
-# Can't use bash -i, if you want to launch editor.sh( "${EDITOR}" ) from Makefile( "make -f Makefile" ).
+# Can't use bash -i, if you want to launch ~/editor.sh(the setting value of environment variable EDITOR) from Makefile( "make -f Makefile" ).
+# Interactive shells (bash -i) are not guaranteed to work properly from Makefile targets (non-interactive context).
 #!/usr/bin/env -S bash -i
 
 open_files_in_editor "$@"

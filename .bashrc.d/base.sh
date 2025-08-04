ccc4d919ac53967fb6b0812c73b80b98b1d1c99a
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-08-04 13:57:15 +0800

    1

--- .bashrc.d/base.sh
+++ .bashrc.d/base.sh
@@ -422,13 +422,47 @@ time_command() {
 	fi
 }
 
+open_files_in_editor() {
+	case "${HOST_TRIPLE}" in
+		*-cygwin | *-msys | *-mingw* )
+			local translated_file_paths=()
+			local file
+			for file in "$@"; do
+				translated_file_paths+=( "$(cygpath -w "${file}")" )
+			done
+			# echo "${translated_file_paths[@]}"
+
+			# https://stackoverflow.com/questions/10564/how-can-i-set-up-an-editor-to-work-with-git-on-windows
+			# https://docs.github.com/en/get-started/git-basics/associating-text-editors-with-git
+			# https://git-scm.com/book/ms/v2/Getting-Started-First-Time-Git-Setup
+			# https://npp-user-manual.org/docs/command-prompt/
+			# Can't specify -notabbar if you want to open multiple files
+			"$(cygpath -u 'C:\Program Files\Notepad++\notepad++.exe')" -multiInst -nosession -noPlugin "${translated_file_paths[@]}"
+
+			# https://stackoverflow.com/questions/30024353/how-can-i-use-visual-studio-code-as-default-editor-for-git
+			# https://docs.github.com/en/get-started/git-basics/associating-text-editors-with-git
+			# https://code.visualstudio.com/docs/configure/command-line
+			# "$(cygpath -u 'C:\Program Files\Microsoft VS Code\Code.exe')" --wait --new-window "${translated_file_paths[@]}"
+			;;
+		*-linux* )
+			# code --wait --new-window "$@"
+			# gedit "$@"
+			kwrite "$@"
+			;;
+		* )
+			echo "unknown host : ${HOST_TRIPLE}"
+			return 1
+			;;
+	esac
+}
+
 show_command_output_in_editor() {
 	local output="$1"
 	shift 1
 
 	time_command "$@" >"${output}" 2>&1
 
-	"${EDITOR}" "${output}"
+	open_files_in_editor "${output}"
 
 	rm -rf "${output}"
 }
--- editor.sh
+++ editor.sh
@@ -1,37 +1,8 @@
 #!/usr/bin/env bash
+# simulate bash -i
+source ~/.bashrc
 
-open_files_in_editor() {
-	case "${HOST_TRIPLE}" in
-		*-cygwin | *-msys | *-mingw* )
-			local translated_file_paths=()
-			local file
-			for file in "$@"; do
-				translated_file_paths+=( "$(cygpath -w "${file}")" )
-			done
-			# echo "${translated_file_paths[@]}"
-
-			# https://stackoverflow.com/questions/10564/how-can-i-set-up-an-editor-to-work-with-git-on-windows
-			# https://docs.github.com/en/get-started/git-basics/associating-text-editors-with-git
-			# https://git-scm.com/book/ms/v2/Getting-Started-First-Time-Git-Setup
-			# https://npp-user-manual.org/docs/command-prompt/
-			# Can't specify -notabbar if you want to open multiple files
-			"$(cygpath -u 'C:\Program Files\Notepad++\notepad++.exe')" -multiInst -nosession -noPlugin "${translated_file_paths[@]}"
-
-			# https://stackoverflow.com/questions/30024353/how-can-i-use-visual-studio-code-as-default-editor-for-git
-			# https://docs.github.com/en/get-started/git-basics/associating-text-editors-with-git
-			# https://code.visualstudio.com/docs/configure/command-line
-			# "$(cygpath -u 'C:\Program Files\Microsoft VS Code\Code.exe')" --wait --new-window "${translated_file_paths[@]}"
-			;;
-		*-linux* )
-			# code --wait --new-window "$@"
-			# gedit "$@"
-			kwrite "$@"
-			;;
-		* )
-			echo "unknown host : ${HOST_TRIPLE}"
-			return 1
-			;;
-	esac
-}
+# Can't use bash -i, if you want to launch editor.sh( "${EDITOR}" ) from Makefile( "make -f Makefile" ).
+#!/usr/bin/env -S bash -i
 
 open_files_in_editor "$@"

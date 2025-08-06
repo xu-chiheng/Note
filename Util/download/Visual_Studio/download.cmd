132cfee6fd3c8df612c0a885b2b4481cf6ce7ac1
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-08-07 04:51:33 +0800

    1

--- Util/download/Visual_Studio/download.cmd
+++ Util/download/Visual_Studio/download.cmd
@@ -5,4 +5,4 @@
 @REM https://learn.microsoft.com/en-us/visualstudio/install/create-an-offline-installation-of-visual-studio
 @REM https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
 @REM Visual Studio 2022 Enterprise
-@start mintty.exe --exec bash -i -c 'echo_command rm -rf vslayout; time_command windows_download_executable_from_url_and_execute vs_enterprise.exe https://aka.ms/vs/17/release/vs_enterprise.exe --layout vslayout --all --arch all --lang en-US; read;'
+@start mintty.exe --exec bash -i -c 'time_command windows_download_executable_from_url_and_execute vs_enterprise.exe https://aka.ms/vs/17/release/vs_enterprise.exe --layout vslayout --all --arch all --lang en-US; read;'

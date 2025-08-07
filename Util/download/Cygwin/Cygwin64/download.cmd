4477f5926e0a6c72cb5668b39c0e4460b54d5d7d
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-08-07 19:32:49 +0800

    1

--- Util/download/Cygwin/Cygwin64/download.cmd
+++ Util/download/Cygwin/Cygwin64/download.cmd
@@ -1,7 +1,14 @@
 
-call ../config.cmd
 
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.cygwin.com/setup-x86_64.exe', 'setup-x86_64.exe')"
+@REM set SITE=https://mirrors.163.com/cygwin
+@REM set SITE=https://mirrors.aliyun.com/cygwin
+@REM set SITE=https://mirrors.ustc.edu.cn/cygwin
+set SITE=https://mirrors.tuna.tsinghua.edu.cn/cygwin
+@REM set SITE=https://mirrors.sohu.com/cygwin
+@REM gitk needs X11
+set CATEGORIES=Admin,Archive,Base,Devel,Doc,Editors,Interpreters,Libs,Net,Perl,Python,Security,Shells,System,Tcl,Text,Utils,X11
+
+curl -L -o setup-x86_64.exe https://www.cygwin.com/setup-x86_64.exe
 setup-x86_64.exe --download --quiet-mode --site="%SITE%" --local-package-dir="%CD%" --categories="%CATEGORIES%"
 @REM --include-source
 

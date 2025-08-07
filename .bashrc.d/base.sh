4264e1148e885e4866f5d364670145888128cfd7
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-08-07 21:06:37 +0800

    1

--- .bashrc.d/base.sh
+++ .bashrc.d/base.sh
@@ -305,6 +305,83 @@ ssh-agent_is_ok() {
 	[ -v SSH_AUTH_SOCK ] && [ -v SSH_AGENT_PID ] && [ -S "${SSH_AUTH_SOCK}" ] && quiet_command ps -p "${SSH_AGENT_PID}"
 }
 
+fix_cygwin_connect_gpg_quirk() {
+	# Cygwin has no connect.exe, use MinGW's
+	local usr_bin_connect="/usr/bin/connect.exe"
+	if [ -v MSYS2_DIR ]; then
+		local mingw_ucrt_bin_connect="$(cygpath -u "${MSYS2_DIR}")/ucrt64/bin/connect.exe"
+		if [ -f "${mingw_ucrt_bin_connect}" ]; then
+			if [ ! -f "${usr_bin_connect}" ] || ! cmp --quiet "${usr_bin_connect}" "${mingw_ucrt_bin_connect}" ; then
+				rm -rf "${usr_bin_connect}" \
+				&& cp -f "${mingw_ucrt_bin_connect}" "${usr_bin_connect}"
+			fi
+		fi
+	fi
+
+	# Cygwin has gpg and gpg2 commands, override gpg command to gpg2
+	local usr_bin_gpg="/usr/bin/gpg.exe"
+	local usr_bin_gpg2="/usr/bin/gpg2.exe"
+	if [ -f "${usr_bin_gpg2}" ] ; then
+		if [ -f "${usr_bin_gpg}" ] && [ ! -f "${usr_bin_gpg}".backup ]; then
+			mv -f "${usr_bin_gpg}" "${usr_bin_gpg}".backup
+		fi \
+		&& if [ ! -f "${usr_bin_gpg}" ] || ! cmp --quiet "${usr_bin_gpg}" "${usr_bin_gpg2}" ; then
+			rm -rf "${usr_bin_gpg}" \
+			&& cp -f "${usr_bin_gpg2}" "${usr_bin_gpg}"
+		fi
+	fi
+}
+
+fix_mingw_binmode_commode_quirk() {
+	# https://learn.microsoft.com/en-us/cpp/c-runtime-library/link-options
+
+	local binmode_c='
+#include <fcntl.h>
+int _fmode = _O_BINARY; 
+'
+	local commode_c='
+// #include <corecrt_internal_stdio.h>
+int _commode = 0x0800; // _IOCOMMIT
+'
+	local mingw_root="$(print_mingw_root_dir)"
+	local mode
+	for mode in binmode commode; do
+		local tmp_src="$(mktemp --suffix=.c)"
+		local tmp_obj="$(mktemp --suffix=.o)"
+		local target_obj="${mingw_root}/lib/${mode}.o"
+		echo "${tmp_src}" "${tmp_obj}" "${target_obj}"
+		local varname="${mode}_c"
+		echo "${!varname}" >"${tmp_src}"
+		echo_command cat "${tmp_src}"
+		echo_command gcc -c -O3 -o "${tmp_obj}" "${tmp_src}"
+		echo_command strip --strip-debug --discard-all -o "${tmp_obj}" "${tmp_obj}"
+		echo_command objdump -t "${tmp_obj}"
+		if cmp "${tmp_obj}" "${target_obj}"; then
+			echo "equal"
+		else
+			echo "not equal"
+			rm -rf "${target_obj}" \
+			&& mv -f "${tmp_obj}" "${target_obj}"
+		fi
+		rm -rf "${tmp_src}" "${tmp_obj}"
+	done
+}
+
+fix_cygwin_msys_ssh_quirk() {
+	# ssh uses wrong home directory in Cygwin - Server Fault
+	# https://serverfault.com/questions/95750/ssh-uses-wrong-home-directory-in-cygwin
+
+	# https://linux.die.net/man/1/readlink
+	# https://www.geeksforgeeks.org/readlink-command-in-linux-with-examples/
+	# https://serverfault.com/questions/76042/find-out-symbolic-link-target-via-command-line
+	local home_unix_path="$(cygpath -u "${HOME}")"
+	local ssh_home_dir="/home/${USERNAME}"
+	if ! { [ -e "${ssh_home_dir}" ] && [ "$(readlink -f "${ssh_home_dir}")" = "$(readlink -f "${home_unix_path}")" ] ;}; then
+		rm -rf "${ssh_home_dir}" \
+		&& ln -s "${home_unix_path}" "${ssh_home_dir}"
+	fi
+}
+
 fix_system_quirks_one_time() {
 	if host_triple_is_windows "${HOST_TRIPLE}"; then
 		if [ ! "${HOST_TRIPLE}" = "$(~/config.guess)" ]; then
@@ -313,44 +390,14 @@ fix_system_quirks_one_time() {
 
 		case "${HOST_TRIPLE}" in
 			*-cygwin )
-				# Cygwin has no connect.exe, use MinGW's
-				local usr_bin_connect="/usr/bin/connect.exe"
-				if [ -v MSYS2_DIR ]; then
-					local mingw_ucrt_bin_connect="$(cygpath -u "${MSYS2_DIR}")/ucrt64/bin/connect.exe"
-					if [ -f "${mingw_ucrt_bin_connect}" ]; then
-						if [ ! -f "${usr_bin_connect}" ] || ! cmp --quiet "${usr_bin_connect}" "${mingw_ucrt_bin_connect}" ; then
-							rm -rf "${usr_bin_connect}" \
-							&& cp -f "${mingw_ucrt_bin_connect}" "${usr_bin_connect}"
-						fi
-					fi
-				fi
-
-				# Cygwin has gpg and gpg2 commands, override gpg command to gpg2
-				local usr_bin_gpg="/usr/bin/gpg.exe"
-				local usr_bin_gpg2="/usr/bin/gpg2.exe"
-				if [ -f "${usr_bin_gpg2}" ] ; then
-					if [ -f "${usr_bin_gpg}" ] && [ ! -f "${usr_bin_gpg}".backup ]; then
-						mv -f "${usr_bin_gpg}" "${usr_bin_gpg}".backup
-					fi \
-					&& if [ ! -f "${usr_bin_gpg}" ] || ! cmp --quiet "${usr_bin_gpg}" "${usr_bin_gpg2}" ; then
-						rm -rf "${usr_bin_gpg}" \
-						&& cp -f "${usr_bin_gpg2}" "${usr_bin_gpg}"
-					fi
-				fi
+				time_command fix_cygwin_connect_gpg_quirk
+				;;
+			*-mingw* )
+				time_command fix_mingw_binmode_commode_quirk
+				;;
 		esac
 
-		# ssh uses wrong home directory in Cygwin - Server Fault
-		# https://serverfault.com/questions/95750/ssh-uses-wrong-home-directory-in-cygwin
-
-		# https://linux.die.net/man/1/readlink
-		# https://www.geeksforgeeks.org/readlink-command-in-linux-with-examples/
-		# https://serverfault.com/questions/76042/find-out-symbolic-link-target-via-command-line
-		local home_unix_path="$(cygpath -u "${HOME}")"
-		local ssh_home_dir="/home/${USERNAME}"
-		if ! { [ -e "${ssh_home_dir}" ] && [ "$(readlink -f "${ssh_home_dir}")" = "$(readlink -f "${home_unix_path}")" ] ;}; then
-			rm -rf "${ssh_home_dir}" \
-			&& ln -s "${home_unix_path}" "${ssh_home_dir}"
-		fi
+		time_command fix_cygwin_msys_ssh_quirk
 	fi
 }
 

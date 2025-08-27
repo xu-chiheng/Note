056f564852c520a1738479ba261b257407a8c309
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-08-27 17:13:57 +0800

    1

--- Tool/clean.sh
+++ Tool/clean.sh
@@ -3,30 +3,34 @@
 cd "$(dirname "$0")"
 . "./common.sh"
 
-FIND_ARGS=(
-	-mindepth 1
-	-maxdepth 1
-	'('
-		-name '*output.txt'
+clean() {
+	local find_args=(
+		-mindepth 1
+		-maxdepth 1
+		'('
+			-name '*output.txt'
 
-		-or -name 'a.exe'
-		-or -name 'a.out'
-		-or -name 'main.cpp'
-		-or -name 'main.c'
+			-or -name 'a.exe'
+			-or -name 'a.out'
+			-or -name 'main.cpp'
+			-or -name 'main.c'
 
-		-or -name '*-build'
-		-or -name '*-install'
-	')'
-	-and
-	-not
-	'('
-		# remove manually
-		-name '*.sha512'
-		-or -name '*.cmd'
-		-or -name '*.sh'
-	')'
-)
+			-or -name '*-build'
+			-or -name '*-install'
+		')'
+		-and
+		-not
+		'('
+			# remove manually
+			-name '*.sha512'
+			-or -name '*.cmd'
+			-or -name '*.sh'
+		')'
+	)
 
-# Windows also has a find command
-# /cygdrive/c/Windows/system32/find
-/usr/bin/find "${FIND_ARGS[@]}" -print0 | xargs -0 -n100 rm -rf
+	# Windows also has a find command
+	# /cygdrive/c/Windows/system32/find
+	/usr/bin/find "${find_args[@]}" -print0 | xargs -0 -n100 rm -rf
+}
+
+clean "$@"

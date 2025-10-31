3e4dd263a91b4e6b1cf75c496d04043347220bed
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-10-31 18:03:06 +0800

    1

---
+++ Util/sing-box-yg.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S bash -i
+
+# Sing-box精装桶四合一协议VPS专用脚本：三大独家功能！自签/acme双证书切换、Argo固定临时双隧道（可共存）、Psiphon赛风VPN（30个国家）分流功能。Hostuno三合一代理脚本
+# https://github.com/yonggekkk/sing-box-yg
+
+____how_to_use_this_script____() {
+	# VPS SSH login as root
+	cd ~
+	rm -rf .git Note .bashrc.d Util
+	git clone https://github.com/xu-chiheng/Note -b main
+	mv Note/.git ./
+	rm -rf Note
+	git reset --hard HEAD
+	Util/sing-box-yg.sh
+}
+unset -f ____how_to_use_this_script____
+
+setup() {
+	local install_script="https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/sb.sh"
+	wget -qO- "${install_script}" | bash
+	# curl -Ls "${install_script}" | bash
+}
+
+setup

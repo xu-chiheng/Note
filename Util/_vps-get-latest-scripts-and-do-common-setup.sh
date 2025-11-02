791c73b0a04d7c68a2b61e9cafd92726c6efb80a
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-11-02 22:58:23 +0800

    1

--- Util/_vps-get-latest-scripts-and-do-common-setup.sh
+++ Util/_vps-get-latest-scripts-and-do-common-setup.sh
@@ -5,3 +5,6 @@ git clone https://github.com/xu-chiheng/Note -b main
 mv Note/.git ./
 rm -rf Note
 git reset --hard HEAD
+bash -i -c "time_command linux_server_common_setup"
+echo "Rebooting... Please reconnect using SSH"
+reboot

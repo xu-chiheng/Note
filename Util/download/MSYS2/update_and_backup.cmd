79024fd731fc25f3ccad21e1d12fd9f5b520b63b
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2025-11-05 23:45:00 +0800

    1

--- Util/download/MSYS2/update_and_backup.cmd
+++ Util/download/MSYS2/update_and_backup.cmd
@@ -3,4 +3,4 @@
 @set PATH="%MSYS2_DIR%\usr\bin";%PATH%
 @set MSYSTEM=MSYS
 
-@start mintty.exe --exec bash -i update_and_backup.sh
+@start mintty.exe --exec bash -i -c "msys2_update_and_backup; read;"

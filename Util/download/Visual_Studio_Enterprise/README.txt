5db4aef26d2d6a4d7f18af1b3dd508d90dfe9044
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2026-01-16 01:17:14 +0800

    1

---
+++ Util/download/Visual_Studio_Enterprise/README.txt
@@ -0,0 +1,2 @@
+https://learn.microsoft.com/en-us/visualstudio/install/create-an-offline-installation-of-visual-studio
+https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
--- Util/download/Visual_Studio_Enterprise/download_vs2022.cmd
+++ Util/download/Visual_Studio_Enterprise/download_vs2022.cmd
@@ -2,8 +2,6 @@
 
 @set PATH="%CYGWIN_DIR%\bin";%PATH%
 
-@REM https://learn.microsoft.com/en-us/visualstudio/install/create-an-offline-installation-of-visual-studio
-@REM https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
 @REM Visual Studio 2022 Enterprise
 @REM https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-history
 @start mintty.exe --exec bash -i -c 'time_command windows_download_executable_from_url_and_execute vs2022_enterprise.exe https://aka.ms/vs/17/release/vs_enterprise.exe --layout vs2022 --all --arch all --lang en-US; read;'

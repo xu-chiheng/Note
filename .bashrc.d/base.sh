be46a3597848a57b8facf533922a81a1595503df
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2026-03-09 07:29:14 +0800

    1

--- .bashrc.d/base.sh
+++ .bashrc.d/base.sh
@@ -502,9 +502,16 @@ ensure_cygwin_msys_ssh_home_dir_point_to_real_home() {
 	# https://serverfault.com/questions/76042/find-out-symbolic-link-target-via-command-line
 	if host_triple_is_windows; then
 		local ssh_home_dir="/home/${USERNAME}"
-		if [ ! "${ssh_home_dir}" = ~ ] && ! { [ -e "${ssh_home_dir}" ] && [ "$(readlink -f "${ssh_home_dir}")" = "$(readlink -f ~)" ] ;}; then
-			rm -rf "${ssh_home_dir}" \
-			&& ln -s ~ "${ssh_home_dir}"
+		if [ ! "${ssh_home_dir}" = ~ ]; then
+			if ! { [ -L "${ssh_home_dir}" ] && [ "$(readlink -f "${ssh_home_dir}")" = "$(readlink -f ~)" ] ;}; then
+				rm -rf "${ssh_home_dir}" \
+				&& ln -s ~ "${ssh_home_dir}"
+			fi
+		else
+			if [ ! -d "${ssh_home_dir}" ]; then
+				rm -rf "${ssh_home_dir}" \
+				&& mkdir -p "${ssh_home_dir}"
+			fi
 		fi
 	fi
 }
